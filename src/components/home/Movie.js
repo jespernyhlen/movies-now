be66a5c52fb99dcaf2e69577c713e7b6a9898260
Jesper Nyhlen <jeppe_nyhlen@hotmail.com>
2019-11-03 00:22:27 +0100

    Fixed images trying to load before defined

--- src/components/home/Movie.js
+++ src/components/home/Movie.js
@@ -42,128 +42,121 @@ class Movie extends Component {
 
         return (
             <React.Fragment>
-                {!movie.original_title ? (
-                    <div id='loading-bar-spinner' className='spinner'>
-                        <div className='spinner-icon'></div>
-                    </div>
-                ) : (
-                    <div>
-                        <div
-                            className='container'
-                            style={{
-                                maxWidth: '1620px !important',
-                                background: '#000'
-                            }}
-                        >
-                            <div className='row center-container'>
-                                <div className='row movie-row'>
-                                    <div
-                                        className={
-                                            !this.state.image2Loaded
-                                                ? 'col-md-4 movie-transition'
-                                                : 'col-md-4'
+                <div>
+                    <div
+                        className='container'
+                        style={{
+                            maxWidth: '1620px !important',
+                            background: '#000'
+                        }}
+                    >
+                        <div className='row center-container'>
+                            <div className='row movie-row'>
+                                <div
+                                    className={
+                                        !this.state.image2Loaded
+                                            ? 'col-md-4 movie-transition'
+                                            : 'col-md-4'
+                                    }
+                                >
+                                    <img
+                                        onLoad={() =>
+                                            this.setState({
+                                                image2Loaded: true
+                                            })
                                         }
-                                    >
-                                        <img
-                                            onLoad={() =>
-                                                this.setState({
-                                                    image2Loaded: true
-                                                })
-                                            }
-                                            className='thumbnail w-100'
-                                            src={
-                                                'https://image.tmdb.org/t/p/w400/' +
-                                                movie.poster_path
-                                            }
-                                            alt='Poster'
-                                        />
+                                        className='thumbnail w-100'
+                                        src={
+                                            !movie.original_title
+                                                ? null
+                                                : 'https://image.tmdb.org/t/p/w400/' +
+                                                  movie.poster_path
+                                        }
+                                        alt='Poster'
+                                    />
+                                </div>
+                                <div className='col-md-8'>
+                                    <div className='inline-heading mb-3'>
+                                        <h1 className=' font-weight-bold text-light'>
+                                            {movie.original_title}
+                                        </h1>
+                                        <h4 className='font-weight-bold text-light-transp'>
+                                            {movie.release_date
+                                                ? movie.release_date.slice(0, 4)
+                                                : null}
+                                        </h4>
                                     </div>
-                                    <div className='col-md-8'>
-                                        <div className='inline-heading mb-3'>
-                                            <h1 className=' font-weight-bold text-light'>
-                                                {movie.original_title}
-                                            </h1>
-                                            <h4 className='font-weight-bold text-light-transp'>
-                                                {movie.release_date
-                                                    ? movie.release_date.slice(
-                                                          0,
-                                                          4
-                                                      )
-                                                    : null}
+
+                                    <ul className='list-group'>
+                                        <li className=''>
+                                            <h4 className='text-light-transp font-weight-light'>
+                                                {genreString.slice(0, -2)}
                                             </h4>
+                                        </li>
+                                        <div className='text-light movie-desc mt-3'>
+                                            <div className=''>
+                                                {movie.overview
+                                                    ? movie.overview.length >
+                                                      443
+                                                        ? movie.overview.substring(
+                                                              0,
+                                                              442
+                                                          ) + '...'
+                                                        : movie.overview
+                                                    : null}
+                                            </div>
                                         </div>
 
-                                        <ul className='list-group'>
-                                            <li className=''>
-                                                <h4 className='text-light-transp font-weight-light'>
-                                                    {genreString.slice(0, -2)}
-                                                </h4>
-                                            </li>
-                                            <div className='text-light movie-desc mt-3'>
-                                                <div className=''>
-                                                    {movie.overview
-                                                        ? movie.overview
-                                                              .length > 443
-                                                            ? movie.overview.substring(
-                                                                  0,
-                                                                  442
-                                                              ) + '...'
-                                                            : movie.overview
-                                                        : null}
-                                                </div>
+                                        <li className='row mt-5 text-light'>
+                                            <div className='col-md-2 movie-icon'>
+                                                <i className='far fa-clock text-primary'></i>{' '}
+                                                {movie.runtime
+                                                    ? movie.runtime + ' min'
+                                                    : ' ?'}
                                             </div>
 
-                                            <li className='row mt-5 text-light'>
-                                                <div className='col-md-2 movie-icon'>
-                                                    <i className='far fa-clock text-primary'></i>{' '}
-                                                    {movie.runtime
-                                                        ? movie.runtime + ' min'
-                                                        : ' ?'}
-                                                </div>
-
-                                                <div className='col-md-2 movie-icon'>
-                                                    <i className='fas fa-star text-warning'></i>{' '}
-                                                    <strong>
-                                                        {movie.vote_average}
-                                                    </strong>{' '}
-                                                    {' / ' + movie.vote_count}
-                                                </div>
-                                                <div className='col-md-2 movie-icon'>
-                                                    <i className='fab fa-imdb text-warning'></i>{' '}
-                                                    <a
-                                                        href={
-                                                            'https://www.imdb.com/title/' +
-                                                            movie.imdb_id
-                                                        }
-                                                        target='_blank'
-                                                        rel='noopener noreferrer'
-                                                        className='movie-imdb-link'
-                                                    >
-                                                        IMDB
-                                                    </a>
-                                                </div>
-                                                <div className='col-md-2 movie-icon'>
-                                                    <i className='far fa-comment text-success'></i>{' '}
-                                                    {movie.original_language
-                                                        ? movie.original_language.toUpperCase()
-                                                        : ' ?'}
-                                                </div>
-                                            </li>
-                                            <Link
-                                                to={pathname}
-                                                className='col-md-5 btn btn-default mt-5 movie-back-btn'
-                                            >
-                                                <i className='far fa-arrow-alt-circle-left'></i>{' '}
-                                                Go Back To Search
-                                            </Link>
-                                        </ul>
-                                    </div>
+                                            <div className='col-md-2 movie-icon'>
+                                                <i className='fas fa-star text-warning'></i>{' '}
+                                                <strong>
+                                                    {movie.vote_average}
+                                                </strong>{' '}
+                                                {' / ' + movie.vote_count}
+                                            </div>
+                                            <div className='col-md-2 movie-icon'>
+                                                <i className='fab fa-imdb text-warning'></i>{' '}
+                                                <a
+                                                    href={
+                                                        'https://www.imdb.com/title/' +
+                                                        movie.imdb_id
+                                                    }
+                                                    target='_blank'
+                                                    rel='noopener noreferrer'
+                                                    className='movie-imdb-link'
+                                                >
+                                                    IMDB
+                                                </a>
+                                            </div>
+                                            <div className='col-md-2 movie-icon'>
+                                                <i className='far fa-comment text-success'></i>{' '}
+                                                {movie.original_language
+                                                    ? movie.original_language.toUpperCase()
+                                                    : ' ?'}
+                                            </div>
+                                        </li>
+                                        <Link
+                                            to={pathname}
+                                            className='col-md-5 btn btn-default mt-5 movie-back-btn'
+                                        >
+                                            <i className='far fa-arrow-alt-circle-left'></i>{' '}
+                                            Go Back To Search
+                                        </Link>
+                                    </ul>
                                 </div>
                             </div>
-                            <div className='row'></div>
                         </div>
+                        <div className='row'></div>
                     </div>
-                )}
+                </div>
                 <div
                     className={
                         !this.state.image1Loaded
